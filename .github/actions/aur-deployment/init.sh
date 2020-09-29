423e738cd8e5c5bde1064e3e0c697f61fdcff354
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-29 15:42:06 -0500

    Added some test to the workflow which deploy to AUR.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- .github/actions/aur-deployment/init.sh
+++ .github/actions/aur-deployment/init.sh
@@ -5,3 +5,7 @@ echo "Hello #2 ${ISRAEL}"
 
 find / -iname "arch-aur" 2> /dev/null
 find / -iname "PKGBUILD" 2> /dev/null
+
+ls -lha /github/
+ls -lha /github/workspace/
+ls -lha /github/workspace/arch-aur/
