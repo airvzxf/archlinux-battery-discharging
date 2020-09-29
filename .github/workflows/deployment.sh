42d34f8473449b2da66101b50a798efd6fad93e7
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-28 22:32:01 -0500

    Added some test to the workflow which deploy to AUR.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

---
+++ .github/workflows/deployment.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -evx
+
+aur_project="battery-discharging-beep-git"
+
+ssh_path="${HOME}/.ssh/"
+ssh_config="${HOME}/.ssh/config"
+ssh_aur="${HOME}/.ssh/aur"
+deploy_path="${HOME}/AUR/"
+aur_package="${WORKSPACE_PATH}/arch-aur/"
+
+rm -f "${ssh_config}"
+rm -f "${ssh_aur}"
+rm -fR "${deploy_path}"
+
+mkdir -p "${ssh_path}"
+
+if [ ! -f "${ssh_config}" ]; then
+  echo "Created the ssh config file."
+  touch "${ssh_config}"
+fi
+
+if ! grep -i "Host aur.archlinux.org" &> /dev/null < "${ssh_config}"; then
+  echo "Added host to the ssh config file."
+  echo -e "Host aur.archlinux.org\n  IdentityFile ${ssh_aur}\n  User aur\n" >> "${ssh_config}"
+fi
+
+echo "Added the private key into the AUR file."
+echo "${SSH_KEY}" > "${ssh_aur}"
+
+echo "SSH Config:"
+cat "${ssh_config}"
+cat "${ssh_aur}"
+echo "ssh_key: ${SSH_KEY}"
+
+cd "${HOME}" || exit
+mkdir -p "${deploy_path}"
+cd "${deploy_path}" || exit
+git clone "ssh://aur@aur.archlinux.org/${aur_project}.git"
+cd "${aur_project}" || exit
+pwd
+cp -f "${aur_package}"* .
+./upload-package-automatic.sh
+
+cd "${HOME}" || exit
+rm -f "${ssh_config}"
+rm -f "${ssh_aur}"
+rm -fR "${deploy_path}"
+ls -lha "${ssh_path}"
+ls "${HOME}"
--- arch-aur/upload-package-automatic.sh
+++ arch-aur/upload-package-automatic.sh
@@ -7,5 +7,5 @@ makepkg --printsrcinfo > .SRCINFO
 git diff --exit-code
 git add .
 git status
-git commit -m "Automatic deployment coming from the official repository in GitHub using CI (Continuous Integration)."
-git push
+#git commit -m "Automatic deployment coming from the official repository in GitHub using CI (Continuous Integration)."
+#git push
