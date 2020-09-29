d59360c2268c317b129396ddc93cee14149441b0
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-28 22:40:36 -0500

    Added some test to the workflow which deploy to AUR.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- .github/workflows/deployment.sh
+++ .github/workflows/deployment.sh
@@ -6,7 +6,7 @@ ssh_path="${HOME}/.ssh/"
 ssh_config="${HOME}/.ssh/config"
 ssh_aur="${HOME}/.ssh/aur"
 deploy_path="${HOME}/AUR/"
-aur_package="${{ github.workspace }}/arch-aur/"
+aur_package="${github.workspace}/arch-aur/"
 exit 1
 rm -f "${ssh_config}"
 rm -f "${ssh_aur}"
@@ -25,12 +25,12 @@ if ! grep -i "Host aur.archlinux.org" &> /dev/null < "${ssh_config}"; then
 fi
 
 echo "Added the private key into the AUR file."
-echo "${{ secrets.SSH_PRIVATE_KEY }}" > "${ssh_aur}"
+echo "${secrets.SSH_PRIVATE_KEY}" > "${ssh_aur}"
 
 echo "SSH Config:"
 cat "${ssh_config}"
 cat "${ssh_aur}"
-echo "ssh_key: ${{ secrets.SSH_PRIVATE_KEY }}"
+echo "ssh_key: ${secrets.SSH_PRIVATE_KEY}"
 
 cd "${HOME}" || exit
 mkdir -p "${deploy_path}"
