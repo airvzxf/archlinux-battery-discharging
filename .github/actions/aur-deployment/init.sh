b82dc1c6804a2dd07c13cf8d53d11b1c176e9102
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-29 15:46:36 -0500

    Added some test to the workflow which deploy to AUR.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- .github/actions/aur-deployment/init.sh
+++ .github/actions/aur-deployment/init.sh
@@ -1,9 +1,4 @@
 #!/bin/sh -ex
 
-echo "Hello #1 ${SSH_PUBLIC_KEY}"
-echo "Hello #2 ${ISRAEL}"
-
-find / -iname "arch-aur" 2> /dev/null
-find / -iname "PKGBUILD" 2> /dev/null
-
-ls -lhaR /github/
+echo "SSH_PUBLIC_KEY: ${SSH_PUBLIC_KEY}"
+echo "GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}"
