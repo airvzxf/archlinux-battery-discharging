c56d4fc5515c5285d20d313089791f8e747a7ce3
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-29 15:28:19 -0500

    Added some test to the workflow which deploy to AUR.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- .github/actions/aur-deployment/init.sh
+++ .github/actions/aur-deployment/init.sh
@@ -2,7 +2,9 @@
 
 echo "Hello!"
 echo "Hello ${1}"
-echo "Hello #1 ${INPUT_WHO-TO-GREET}"
-echo "Hello #2 ${input_who-to-greet}"
+echo "Hello #1 ${SSH_PUBLIC_KEY}"
+echo "Hello #2 ${ssh_public_key}"
+echo "Hello #3 ${ISRAEL}"
+echo "Hello #4 ${israel}"
 
 printenv
