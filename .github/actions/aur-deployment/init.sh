280a6459a29ddf792e1040e7d496769f6a630e93
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-29 15:18:54 -0500

    Added some test to the workflow which deploy to AUR.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- .github/actions/aur-deployment/init.sh
+++ .github/actions/aur-deployment/init.sh
@@ -1,8 +1,8 @@
-#!/bin/sh -exv
+#!/bin/sh -e
 
 echo "Hello!"
 echo "Hello ${1}"
 echo "Hello #1 ${INPUTS_WHO_TO_GREET}"
 echo "Hello #2 ${inputs_who_to_greet}"
-time=$(date)
-echo "::set-output name=time::${time}"
+
+printenv
