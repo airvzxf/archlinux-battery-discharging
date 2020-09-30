cce68304781449717c1eb66dbd6509152cd33ddc
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-29 21:06:21 -0500

    Added some test to the workflow which deploy to AUR.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- .github/actions/aur-deployment/deployment.sh
+++ .github/actions/aur-deployment/deployment.sh
@@ -56,6 +56,7 @@ chown -R "${user}":"${user}" "${deploy_path}"
 ls -lha .
 
 cd "${deploy_path}" || exit
+ping -c 2 google.com
 git clone -vvvv "ssh://aur@aur.archlinux.org/${aur_project}.git"
 ls -lha .
 exit 0
