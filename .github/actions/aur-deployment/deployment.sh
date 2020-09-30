2ff5fc7576b5c2a2d15ecbcb12a40cdbe46007f3
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-29 19:15:04 -0500

    Added some test to the workflow which deploy to AUR.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

--- .github/actions/aur-deployment/deployment.sh
+++ .github/actions/aur-deployment/deployment.sh
@@ -9,11 +9,10 @@ ssh_aur_public="/root/.ssh/aur.pub"
 deploy_path="/root/AUR/"
 aur_package="${GITHUB_WORKSPACE}/arch-aur/"
 
-
 whoami
 useradd -m immortal
 
-echo -e "a\na" | sudo passwd immortal
+echo -e "a\na" | passwd immortal
 
 echo "a" | su - immortal -c "whoami; echo HOME: \${HOME}; ls -lha ~;exit;"
 
